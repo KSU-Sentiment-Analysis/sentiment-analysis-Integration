c59a53fa57d4805ca3d6d9b83986e54905a54f66
Aravind Iyer <aravind030702@gmail.com>
2025-03-20 10:25:04 -0400

    switching to demo

--- client/src/pages/SentimentAnalysis.js
+++ client/src/pages/SentimentAnalysis.js
@@ -103,6 +103,23 @@ const SentimentAnalysis = () => {
                     </Paper>
                 </Grid>
             </Grid>
+            {/* Embed Power BI Report */}
+            <Grid container spacing={3} sx={{ mt: 4 }}>
+                <Grid item xs={12}>
+                    <Paper sx={{ p: 2, textAlign: "center" }}>
+                        <Typography variant="h6">Sentiment Analysis Report</Typography>
+                        <p>Loading Power BI Report...</p> {/* This should be visible */}
+                        <iframe
+                            title="Power BI Report"
+                            width="100%"
+                            height="600"
+                            src="https://app.powerbi.com/groups/me/reports/4a3f4170-1d6b-4d17-a12b-df8001e98854/b32f1b35ac0d00ad53d6?ctid=45f26ee5-f134-439e-bc93-e6c7e33d61c2&pbi_source=shareVisual&visual=150aa6ce378b3c0035c2&height=388.06&width=374.31&bookmarkGuid=c1e5062a-228c-4d22-88f7-95e25aa3907d"
+                            frameBorder="0"
+                            allowFullScreen
+                        ></iframe>
+                    </Paper>
+                </Grid>
+            </Grid>
 
             {/* Download Processed CSV File */}
             {processedFile && (
